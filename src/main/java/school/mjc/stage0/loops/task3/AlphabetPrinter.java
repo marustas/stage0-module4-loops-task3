32f0012f03afa4fb8fcf52ad54137223c7e17c3a
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 15:39:24 +0300

    third try

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         char c;
-        for (c = 'a'; c <= 'a'; ++c)
+        for (c = 'a'; c <= 'z'; ++c)
             System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,12 +3,23 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t) {
         int sum = 0;
-        while (t != 0) {
-            int digit;
-            digit = t % 10;
-            sum = sum + digit;
-            t = t / 10;
+        if (t >= 0) {
+            while (t != 0) {
+                int digit;
+                digit = t % 10;
+                sum = sum + digit;
+                t = t / 10;
+            }
+            System.out.println(sum);
+        } else if (t < 0) {
+            while (t != 0) {
+                int digit;
+                digit = t % 10;
+                sum = sum + digit;
+                t = t / 10;
+            }
+            System.out.println(-1 * sum);
         }
-        System.out.println(sum);
+
     }
 }
