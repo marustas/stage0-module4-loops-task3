d324518bcb9df4375be18c845826a062cd90f945
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 15:26:06 +0300

    first try

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        long n, i, t = 9;
+        long i, t = 9;
         int sum = 0;
         for (i = 1; i <= lengthOfLastNumber; i++) {
             sum += t;
